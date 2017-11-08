7601d6939cb2da6ad327f0f93e40cbb2f1a84b8b
anthonyyang48 <wangrusherry12@gmail.com>
2017-11-08 03:54:48 +0000

    New Changes

---
+++ js/guts.js
@@ -0,0 +1,40 @@
+function divide(x,y)
+{
+  return x/y;
+}
+
+function multiply(x,y)
+{
+    return x*y;
+}
+
+function subtract(x,y)
+{
+    return x-y;
+}
+
+function add(x,y)
+{
+    return x+y; 
+}
+
+function square_root(x)
+{
+    return Math.sqrt(x);
+}
+
+function power(x,y)
+{
+    return Math.pow(x,y);
+}
+
+function logBase10(x)
+{
+    return Math.log(x);
+}
+
+function generateRandomNumber()
+{
+    return Math.floor((Math.random() * 1000) + 1);
+}
+// You must implement the rest of the functions.
